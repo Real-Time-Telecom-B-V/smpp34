9f74584954073a6b9be12da5ee751994ff491064
Matthias van der Vlies <674985+killdashnine@users.noreply.github.com>
2023-07-15 21:09:41 +0200

    unbind testcases
    Signed-off-by: Matthias van der Vlies <mvdvlies@gmail.com>

--- src/common/commands/unbind.rs
+++ src/common/commands/unbind.rs
@@ -23,11 +23,19 @@ impl unbind {
     /// 
     /// * `header` - already decoded Command Header which is only used for validation as unbind should not have a body
     /// * `pdu` - the complete PDU used for extra validation
-    pub fn decode(header: CommandHeader, _pdu: &Vec<u8>) -> Result<unbind, SmppError> {
-        // TODO check if body is empty
-        Ok(unbind {
-            header,
-        })
+    pub fn decode(header: CommandHeader, pdu: &Vec<u8>) -> Result<unbind, SmppError> {
+        if header.command_id == CommandId::unbind as u32 {
+            if pdu.len() == 16 {
+                Ok(unbind {
+                    header,
+                })
+            } else {
+                Err(SmppError::ESME_ROPTPARNOTALLWD) // Body should be empty
+            }
+        }
+        else {
+            panic!("Passed a non unbind to decode()")
+        }
     }
 
     /// Encode a unbind based on inner command header
@@ -104,11 +112,113 @@ impl unbind_resp {
 }
 
 #[cfg(test)]
-mod unbind_tests {
-
+mod all_unbind_tests {
+    #[cfg(test)]
+    mod unbind_tests {
+        use crate::{CommandHeader, SmppError, unbind, CommandId};
+    
+    
+        #[test]
+        fn encode_unbind() {
+            let unbind = unbind { header: CommandHeader { command_length: 16, command_id: CommandId::unbind as u32, command_status: SmppError::ESME_ROK as u32, sequence_number: 0x12344567 } };
+            let pdu = unbind.encode();
+            assert_eq!(pdu, vec![0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x45, 0x67]);
+        }
+    
+        #[test]
+        fn decode_unbind() {
+            let pdu = vec![0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x45, 0x67];
+            let decoded_command_header = CommandHeader::decode(&pdu).expect("Can not decode command header");
+            let decoded = unbind::decode(decoded_command_header, &pdu).expect("Unable to decode unbind");
+    
+            assert_eq!(decoded.header.command_length, 16);
+            assert_eq!(decoded.header.command_id, CommandId::unbind as u32);
+            assert_eq!(decoded.header.command_status, SmppError::ESME_ROK as u32);
+            assert_eq!(decoded.header.sequence_number, 0x12344567);
+        }
+    
+        #[test]
+        fn decode_unbind_with_body() {
+            let pdu = vec![0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x45, 0x67, 0xde, 0xad, 0xbe, 0xef];
+            let decoded_command_header = CommandHeader::decode(&pdu).expect("Can not decode command header");
+            let decoded = unbind::decode(decoded_command_header, &pdu).unwrap_err();
+            assert_eq!(decoded, SmppError::ESME_ROPTPARNOTALLWD)
+        }
+    
+        #[test]
+        #[should_panic(expected = "Passed a non unbind to decode()")]
+        fn decode_generic_nack_with_invalid_command_id() {
+            let pdu = vec![0x00, 0x00, 0x00, 0x10, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x12, 0x34, 0x45, 0x67];
+            let decoded_command_header = CommandHeader::decode(&pdu).expect("Can not decode command header");
+            unbind::decode(decoded_command_header, &pdu).expect("Unable to decode unbind");
+        }
+    
+        #[test]
+        fn unbind_accept() {
+            let unbind = unbind { header: CommandHeader { command_length: 16, command_id: CommandId::unbind as u32, command_status: SmppError::ESME_ROK as u32, sequence_number: 0x12344567 } };
+            let unbind_resp = unbind.accept();
+    
+            assert_eq!(unbind_resp.header.command_length, 16);
+            assert_eq!(unbind_resp.header.command_id, CommandId::unbind_resp as u32);
+            assert_eq!(unbind_resp.header.command_status, SmppError::ESME_ROK as u32);
+            assert_eq!(unbind_resp.header.sequence_number, 0x12344567);
+        }
+    
+        #[test]
+        fn unbind_reject() {
+            let unbind = unbind { header: CommandHeader { command_length: 16, command_id: CommandId::unbind as u32, command_status: SmppError::ESME_ROK as u32, sequence_number: 0x12344567 } };
+            let unbind_resp = unbind.reject(SmppError::ESME_RINVBNDSTS);
+    
+            assert_eq!(unbind_resp.header.command_length, 16);
+            assert_eq!(unbind_resp.header.command_id, CommandId::unbind_resp as u32);
+            assert_eq!(unbind_resp.header.command_status, SmppError::ESME_RINVBNDSTS as u32);
+            assert_eq!(unbind_resp.header.sequence_number, 0x12344567);
+        }
+    
+        #[test]
+        fn unbind_generic_reject() {
+            let unbind_resp = unbind::generic_reject(0x12344567, SmppError::ESME_RINVBNDSTS);
+    
+            assert_eq!(unbind_resp.header.command_length, 16);
+            assert_eq!(unbind_resp.header.command_id, CommandId::unbind_resp as u32);
+            assert_eq!(unbind_resp.header.command_status, SmppError::ESME_RINVBNDSTS as u32);
+            assert_eq!(unbind_resp.header.sequence_number, 0x12344567);
+        }
+    
+    }
+    
+    #[cfg(test)]
+    mod unbind_resp_tests {
+        use crate::{unbind, SmppError};
+    
+        #[test]
+        fn unbind_resp_is_success() {
+            let unbind_resp = unbind::generic_reject(0x1234567, SmppError::ESME_ROK);
+            assert_eq!(unbind_resp.is_success(), true);
+    
+            let unbind_resp = unbind::generic_reject(0x1234567, SmppError::ESME_RINVBNDSTS);
+            assert_eq!(unbind_resp.is_success(), false);
+        }
+    
+        #[test]
+        fn unbind_resp_command_status() {
+            let unbind_resp = unbind::generic_reject(0x1234567, SmppError::ESME_RINVBNDSTS);
+            assert_eq!(unbind_resp.header.command_status, 0x00000004);
+        }
+    
+        #[test]
+        fn unbind_resp_get_error() {
+            let unbind_resp = unbind::generic_reject(0x1234567, SmppError::ESME_RINVBNDSTS);
+            assert_eq!(unbind_resp.get_error(), SmppError::ESME_RINVBNDSTS);
+        }
+    
+        #[test]
+        fn encode_unbind_resp() {
+            let unbind_resp = unbind::generic_reject(0x12344567, SmppError::ESME_RINVBNDSTS);
+            let pdu = unbind_resp.encode();
+            assert_eq!(pdu, vec![0x00, 0x00, 0x00, 0x10, 0x80, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x04, 0x12, 0x34, 0x45, 0x67]);
+        }
+    
+    }
 }
 
-#[cfg(test)]
-mod unbind_resp_tests {
-
-}
